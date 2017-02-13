c07bf3a9a581f5e4f2e7ea1fd94da2ad63443245
Anil N <anilknyn@yahoo.com>
2017-02-13 23:03:03 +0530

    doxygen regex like in gtkdoc

--- akn-dev.el
+++ akn-dev.el
@@ -1,4 +1,5 @@
 ;;; C and other developmen env.
+(require 'cc-fonts)
 
 ;; Jungo needs breaking of /any/ statement or line to be intended
 ;; by 4 characters (irrespective of the expression parenthesising
@@ -25,14 +26,73 @@
 		     (statement-case-open . +)
 		     (statement-cont . +)
 		     (arglist-intro . c-lineup-arglist-intro-after-paren)
-		     (arglist-cont . +)
-		     (arglist-cont-nonempty . c-lineup-arglist-jungonnn-kludge) ;; Zee kludge
+                     (arglist-cont . +)
+		     (arglist-cont-nonempty . c-lineup-arglist-jungo-kludge) ;; Zee kludge
 		     (arglist-close . c-lineup-arglist)
 		     (inline-open . 0)
 		     (brace-list-open . 0)
 		     (brace-list-entry . 0)
 		     (topmost-intro-cont first c-lineup-topmost-intro-cont c-lineup-gnu-DEFUN-intro-cont))
 		    (c-block-comment-prefix . "")))
+
+;; Shamelessly copied gtkdoc patterns to include "\" prefix and
+;; match keywords on the starting line like, say
+;; /** @brief...
+(defconst doxygen-font-lock-doc-comments
+  (let ((symbol "[a-zA-Z0-9_]+")
+	(header "^ \\* "))
+    `((,(concat header "\\("     symbol "\\):[ \t]*$")
+       1 ,c-doc-markup-face-name prepend nil)
+      (,(concat                  symbol     "()")
+       0 ,c-doc-markup-face-name prepend nil)
+      (,(concat header "\\(" "@" symbol "\\):")
+       1 ,c-doc-markup-face-name prepend nil)
+      (,(concat "[#%@\\]" symbol)
+       0 ,c-doc-markup-face-name prepend nil))
+    ))
+
+(defconst doxygen-font-lock-doc-protection
+  `(("< \\(public\\|private\\|protected\\) >"
+     1 ,c-doc-markup-face-name prepend nil)))
+
+(defconst doxygen-font-lock-keywords
+  `((,(lambda (limit)
+	(c-font-lock-doc-comments "/\\*[*!]<?[ \t]" limit
+	  doxygen-font-lock-doc-comments)
+	(c-font-lock-doc-comments "/\\*[*!]$" limit
+	  doxygen-font-lock-doc-comments)
+	(c-font-lock-doc-comments "/\\*< " limit
+	  doxygen-font-lock-doc-protection)
+	))))
+
+;; Untabified, For auto-newline
+(c-add-style "work" '("jungo"
+		      (c-basic-offset . 4)
+		      (c-comment-only-line-offset 0 . 0)
+		      (c-tab-always-indent . 1)
+		      (indent-tabs-mode . ())
+		      (c-hanging-braces-alist
+		       (defun-open before after)
+		       (defun-close before)
+		       (inline-open before after)
+		       (inline-close before)
+		       (block-open before after)
+		       (block-close before)
+		       (substatement-open before after)
+		       (statement-case-open before after)
+		       (namespace-open before after)
+		       (namespace-close before))
+		      (c-offsets-alist
+		       (arglist-intro . c-lineup-arglist-intro-after-paren)
+		       (arglist-cont . c-lineup-arglist)
+		       (arglist-cont-nonempty . c-lineup-arglist)
+		       (arglist-close . c-lineup-arglist)
+		       (statement-case-open . 0)
+		       (case-label . +))
+                      (c-doc-comment-style .
+                                           ((java-mode . javadoc)
+                                            (pike-mode . autodoc)
+                                            (c-mode . doxygen)))))
 
 ;; Untabified.
 (c-add-style "android" '("java" (c-tab-always-indent . 1) (indent-tabs-mode . ())))
